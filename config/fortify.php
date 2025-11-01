4ab50ec204621b924679adfb0b239e4b1ce335e6
Nurhasdi Nandar <nurhasdy.nandar@gmail.com>
2025-11-01 21:02:55 +0800

    Disable 2FA feature temporarily

--- config/fortify.php
+++ config/fortify.php
@@ -148,11 +148,11 @@ return [
         Features::resetPasswords(),
         Features::updateProfileInformation(),
         Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0,
-        ]),
+        // Features::twoFactorAuthentication([
+        //     'confirm' => true,
+        //     'confirmPassword' => true,
+        //     // 'window' => 0,
+        // ]),
     ],
 
 ];
